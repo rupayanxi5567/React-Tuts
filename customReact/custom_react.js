ab03f8aad7a3ac18dde9d582ffb7500c3ac2685f
rupayanxi5567 <rupayandeyxi@gmail.com>
2023-12-20 19:02:42 +0530

    custom react app done

---
+++ customReact/custom_react.js
@@ -0,0 +1,1 @@
+let main_container_root = document.getElementById("root")
---
+++ tut_1/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chaiaur"
+
+
+function App() {
+
+  let chai_eater = "fotyy"
+
+  return (
+  <> 
+    <Chai />
+    <h1>Let's drink chai my {chai_eater} </h1>
+  </>
+  )
+}
+
+export default App
