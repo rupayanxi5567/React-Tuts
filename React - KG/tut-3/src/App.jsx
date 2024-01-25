4a211d2c4b00e96c8cbac85a9304405e5dcdb762
rupayanxi5567 <rupayandeyxi@gmail.com>
2024-01-25 17:53:53 +0530

    button create done

--- React - KG/tut-3/src/App.jsx
+++ React - KG/tut-3/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FotyButton from './FotyButton'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1>This is Kg react with bulu + foty</h1>
+      <FotyButton/>
     </>
   )
 }
---
+++ React - KG/tut-3/src/FotyButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FotyButton = () => {
+  return (
+    <div>
+      <button>Tap here</button>
+    </div>
+  )
+}
+
+export default FotyButton
